19b4597b1bee53780fd665012992f3175cf8d7ca
Rachot Moragraan <moo@ggcity.org>
2023-10-06 17:48:17 -0700

    Building components

--- vite.config.js
+++ vite.config.js
@@ -15,7 +15,7 @@ export default defineConfig({
     rollupOptions: {
       // make sure to externalize deps that shouldn't be bundled
       // into your library
-      external: ['ol', 'ol-mapbox-style', 'mapbox-gl', 'idb-keyval'],
+      external: ['ol', 'ol-mapbox-style', 'mapbox-gl', 'idb-keyval', 'js-yaml'],
     },
   },
 });
